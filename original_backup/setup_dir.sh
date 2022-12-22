d7dc78499ac25e4315b43fa36fd07dead6c6b2b2
alons126 <102910135+alons126@users.noreply.github.com>
2022-12-22 17:57:44 +0200

    Repository creation

---
+++ original_backup/setup_dir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash                                                                                  
+
+echo "Setting up directory structure for MC simulation here"
+mkdir lundfiles mchipo reconhipo rootfiles
+
