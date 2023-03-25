c326cade1061cf8ff10ce8e0a2472590caa5e843
alons126 <102910135+alons126@users.noreply.github.com>
2023-03-25 19:05:14 +0300

    bt3

--- submit/genie_submit/submit_GEMC_Ca48_Q2_0_5_598636MeV.sh
+++ submit/genie_submit/submit_GEMC_Ca48_Q2_0_5_598636MeV.sh
@@ -7,7 +7,7 @@
 #SBATCH --partition=production                                                               
 #SBATCH --time=30:00:00
 #SBATCH --output=/farm_out/%u/%x-%j-%N.out
-#SBATCH --error=//farm_out/%u/%x-%j-%N.err                                                                           
+#SBATCH --error=//farm_out/%u/%x-%j-%N.err
 #SBATCH --array=1-1
 
 NEVENTS=10000
@@ -24,14 +24,6 @@ source /group/clas12/packages/setup.sh
 module load sqlite/dev
 module load clas12/dev
 
-SBATCH --output=/farm_out/%u/%x-%j-%N.out
-#SBATCH --output=/farm_out/%u/%x-%j-%N.out
-##SBATCH --output=/farm_out/%u/%x-%j-%N.out
-
 gemc -USE_GUI=0  -SCALE_FIELD="TorusSymmetric, $TORUS" -SCALE_FIELD="clas12-newSolenoid, -1.0" -N=$NEVENTS -INPUT_GEN_FILE="lund, /lustre19/expphy/volatile/clas12/asportes/simulationFiles/Ca48_G18_10a_02_11b_Q205_598636MeV/lundfiles/${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}.dat" -OUTPUT="hipo, /lustre19/expphy/volatile/clas12/asportes/simulationFiles/Ca48_G18_10a_02_11b_Q205_598636MeV/mchipo/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus$TORUS.hipo" $GCARD
-
-SBATCH --output=/farm_out/%u/%x-%j-%N.out
-#SBATCH --output=/farm_out/%u/%x-%j-%N.out
-##SBATCH --output=/farm_out/%u/%x-%j-%N.out
 
 recon-util -y $YAML -i /lustre19/expphy/volatile/clas12/asportes/simulationFiles/Ca48_G18_10a_02_11b_Q205_598636MeV/mchipo/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo -o /lustre19/expphy/volatile/clas12/asportes/simulationFiles/Ca48_G18_10a_02_11b_Q205_598636MeV/reconhipo/recon_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo
