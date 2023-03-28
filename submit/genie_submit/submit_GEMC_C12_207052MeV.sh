7c55be2bbc1ead3c14a8f05684d0816789211460
alons126 <102910135+alons126@users.noreply.github.com>
2023-03-28 21:07:39 +0300

    rt5

--- submit/genie_submit/submit_GEMC_C12_207052MeV.sh
+++ submit/genie_submit/submit_GEMC_C12_207052MeV.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1                                                                                                   
 #SBATCH --mem-per-cpu=2000                                                                                            
 #SBATCH --account=clas12                                                                                             
-#SBATCH --job-name=C12_G18_10a_02_11b_207052MeV_T4
+#SBATCH --job-name=C12_G18_10a_02_11b_207052MeV_T5
 #SBATCH --partition=production                                                               
 #SBATCH --time=30:00:00
 #SBATCH --output=/farm_out/%u/%x-%j-%N.out
@@ -21,10 +21,10 @@ YAML=/u/home/asportes/clas12simulations/simulationJ/submit/rgm_mc.yaml
 
 source /etc/profile.d/modules.sh
 source /group/clas12/packages/setup.sh
-module load sqlite/4.4.1
-module load clas12/pro
-# module load sqlite/dev
-# module load clas12/dev
+# module load sqlite/4.4.1
+# module load clas12/pro
+module load sqlite/dev
+module load clas12/dev
 
 gemc -USE_GUI=0  -SCALE_FIELD="TorusSymmetric, $TORUS" -SCALE_FIELD="clas12-newSolenoid, -1.0" -N=$NEVENTS -INPUT_GEN_FILE="lund, /lustre19/expphy/volatile/clas12/asportes/simulationFiles/C12_G18_10a_02_11b_207052MeV/lundfiles/${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}.dat" -OUTPUT="hipo, /lustre19/expphy/volatile/clas12/asportes/simulationFiles/C12_G18_10a_02_11b_207052MeV/mchipo/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus$TORUS.hipo" $GCARD
 
