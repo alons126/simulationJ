f866a7033eead71d768d2eb5b07d728c9d60d6bc
alons126 <102910135+alons126@users.noreply.github.com>
2023-08-01 19:36:55 +0300

    C12_G18_02a_00_000_Q208_598636MeV lund test 1

--- generate-genieLUNDfiles.sh
+++ generate-genieLUNDfiles.sh
@@ -29,6 +29,8 @@ root 'GENIE_to_LUND.C("/lustre19/expphy/volatile/clas12/asportes/truthLevelROOTF
 root 'GENIE_to_LUND.C("/lustre19/expphy/volatile/clas12/asportes/truthLevelROOTFiles/C12_G18_10a_02_11b_207052MeV/C12_G18_10a_02_11b_207052MeV.root", "C12_G18_10a_02_11b_207052MeV", 1000, "1-foil", 6, 6)'
 # C12_G18_10a_02_11b_402962MeV:
 root 'GENIE_to_LUND.C("/lustre19/expphy/volatile/clas12/asportes/truthLevelROOTFiles/C12_G18_10a_02_11b_402962MeV/C12_G18_10a_02_11b_402962MeV.root","C12_G18_10a_02_11b_402962MeV",1000,"1-foil",12,6)'
+# C12_G18_02a_00_000_Q208_598636MeV:
+root 'GENIE_to_LUND.C("/lustre19/expphy/volatile/clas12/asportes/truthLevelROOTFiles/C12_G18_02a_00_000_Q208_598636MeV/11_1000060120_EM_5_98636GeV_G18_02a_00_000_Q2_0_8.gst.root","C12_G18_02a_00_000_Q208_598636MeV",1000,"1-foil",12,6)'
 
 
 # Ar40_G18_10a_02_11b_207052MeV:
