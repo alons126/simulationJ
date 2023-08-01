8a19ec6b7cf5c1dce762297bd6dbbc015c068ffe
alons126 <102910135+alons126@users.noreply.github.com>
2023-08-01 19:44:15 +0300

    C12_G18_02a_00_000_Q208_598636MeV GEMC test 1

--- submit/genie_submit/submit_GEMC_C12_G18_Q208_598636MeV.sh
+++ submit/genie_submit/submit_GEMC_C12_G18_Q208_598636MeV.sh
@@ -3,16 +3,16 @@
 #SBATCH --ntasks=1                                                                                                   
 #SBATCH --mem-per-cpu=2000                                                                                            
 #SBATCH --account=clas12                                                                                             
-#SBATCH --job-name=mc_rgm_gcf                                                                                              
+#SBATCH --job-name=C12_G18_02a_00_000_Q208_598636MeV_T1
 #SBATCH --partition=production                                                               
 #SBATCH --time=20:00:00                                                                                               
 #SBATCH --output=/farm_out/%u/%x-%j-%N.out                                                                           
 #SBATCH --error=//farm_out/%u/%x-%j-%N.err                                                                           
-#SBATCH --array=1-1000                                                                                                
+#SBATCH --array=1-1
 
 NEVENTS=10000
 TORUS=-1.0
-FILE_PREFIX=qe_c_6gev #Change file prefix for your simulation
+FILE_PREFIX=C12_G18_02a_00_000_Q208_598636MeV #Change file prefix for your simulation
 
 GCARD=rgm.gcard
 YAML=rgm_mc.yaml
@@ -25,6 +25,6 @@ module load sqlite/dev
 #module load clas12/pro
 # I had to switch to the software versions Justin uses to avoid a seg fault
 
-gemc -USE_GUI=0  -SCALE_FIELD="TorusSymmetric, $TORUS" -SCALE_FIELD="clas12-newSolenoid, -1.0" -N=10000 -INPUT_GEN_FILE="lund, ../lundfiles/lund_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}.txt" -OUTPUT="hipo, ../mchipo/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus$TORUS.hipo" $GCARD
+gemc -USE_GUI=0  -SCALE_FIELD="TorusSymmetric, $TORUS" -SCALE_FIELD="clas12-newSolenoid, -1.0" -N=10000 -INPUT_GEN_FILE="lund, /lustre19/expphy/volatile/clas12/asportes/simulationFiles/C12_G18_02a_00_000_Q208_598636MeV/lundfiles/lund_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}.txt" -OUTPUT="hipo, /lustre19/expphy/volatile/clas12/asportes/simulationFiles/C12_G18_02a_00_000_Q208_598636MeV/mchipo/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus$TORUS.hipo" $GCARD
 echo FINISHED GEMC
-recon-util -y $YAML -i ../mchipo/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo -o ../reconhipo/recon_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo
+recon-util -y $YAML -i /lustre19/expphy/volatile/clas12/asportes/simulationFiles/C12_G18_02a_00_000_Q208_598636MeV/mchipo/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo -o /lustre19/expphy/volatile/clas12/asportes/simulationFiles/C12_G18_02a_00_000_Q208_598636MeV/reconhipo/recon_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo
