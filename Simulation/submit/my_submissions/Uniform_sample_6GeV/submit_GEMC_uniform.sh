7873a230604f959c0a9e24c705853a678c958b8f
alons126 <102910135+alons126@users.noreply.github.com>
2024-08-19 15:15:38 +0300

    first uniform sample run

--- Simulation/submit/my_submissions/Uniform_sample_6GeV/submit_GEMC_uniform.sh
+++ Simulation/submit/my_submissions/Uniform_sample_6GeV/submit_GEMC_uniform.sh
@@ -30,17 +30,13 @@ YAML=${SUBMIT_SCRIPT_DIR}/rgm_fall2021-cv.yaml
 LUNDOUT=${OUTPATH}/lundfiles
 MCOUT=${OUTPATH}/mchipo
 RECONOUT=${OUTPATH}/reconhipo
-#LUNDOUT=${OUTPATH}/lundfiles/
-#MCOUT=${OUTPATH}/mchipo/
-#RECONOUT=${OUTPATH}/reconhipo/
 
 #source /u/home/asportes/clas12simulations/simulationJ/Simulation/environment_gemc.csh
 source /u/home/asportes/clas12simulations/simulationJ/environment.csh
 #source ../environment_gemc.sh ## original
 
 #SUBMIT GEMC MC
-gemc -USE_GUI=0  -SCALE_FIELD="TorusSymmetric, $TORUS" -SCALE_FIELD="clas12-newSolenoid, -1.0" -N=$NEVENTS -INPUT_GEN_FILE="lund, ${LUNDOUT}/${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}.txt" -OUTPUT="hipo, ${MCOUT}/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus$TORUS.hipo" $GCARD
-#gemc -USE_GUI=0  -SCALE_FIELD="TorusSymmetric, $TORUS" -SCALE_FIELD="clas12-newSolenoid, -1.0" -N=$NEVENTS -INPUT_GEN_FILE="lund, ${LUNDOUT}/lund_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}.txt" -OUTPUT="hipo, ${MCOUT}/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus$TORUS.hipo" $GCARD
+gemc -USE_GUI=0  -SCALE_FIELD="TorusSymmetric, $TORUS" -SCALE_FIELD="clas12-newSolenoid, -1.0" -N=$NEVENTS -INPUT_GEN_FILE="lund, ${LUNDOUT}/lund_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}.txt" -OUTPUT="hipo, ${MCOUT}/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus$TORUS.hipo" $GCARD
 
 #RECONSTRUCTION
 recon-util -y $YAML -n $NEVENTS -i ${MCOUT}/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo -o ${RECONOUT}/recon_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo
