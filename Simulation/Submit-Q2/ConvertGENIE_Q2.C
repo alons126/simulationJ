de7676e35b9a95e9f0c75d8f7554e320cb53175b
alons126 <alonsportes@mail.tau.ac.il>
2024-09-20 17:31:18 +0300

    Q2 test

--- Simulation/Submit-Q2/ConvertGENIE_Q2.C
+++ Simulation/Submit-Q2/ConvertGENIE_Q2.C
@@ -2,7 +2,7 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TRandom3.h"
-//#include "../targets.h"
+// #include "../targets.h"
 #include <fstream>
 #include <iostream>
 
@@ -12,48 +12,48 @@ using namespace std;
 
 /* root -l -q ConvertGENIE_Q2.C */
 
-void ConvertGENIE_Q2() {
-  TString TARGET = "C12";
-  TString GENIE_TUNE = "G18_10a_00_000";
-  TString Q2_CUT = "0_03";
-  TString BEAM_E = "2070MeV";
+void ConvertGENIE_Q2()
+{
+    TString TARGET = "C12";
+    TString GENIE_TUNE = "G18_10a_00_000";
+    TString Q2_CUT = "0_19";
+    TString BEAM_E = "4029MeV";
 
-  int NUM_OF_FILES = 5;
-  string TARGET_TYPE = "4-foil";
-  int TARGET_A = 12;
-  int TARGET_Z = 6;
+    int NUM_OF_FILES = 5;
+    string TARGET_TYPE = "4-foil";
+    int TARGET_A = 12;
+    int TARGET_Z = 6;
 
-  TString TRUTH_SAMPLE_INPUT_DIR = "/w/hallb-scshelf2102/clas12/asportes/2N_Analysis_Truth_Samples/" + TARGET + "/" + GENIE_TUNE + "/Q2_th_test_samples/" + BEAM_E;
-  TString TRUTH_SAMPLE_ROOT_FILE_PREFIX= TARGET + "_" + GENIE_TUNE + "_Q2_" + Q2_CUT + "_" + BEAM_E;
-  TString TRUTH_SAMPLE_ROOT_FILE= TRUTH_SAMPLE_ROOT_FILE_PREFIX + ".root";
+    TString TRUTH_SAMPLE_INPUT_DIR = "/w/hallb-scshelf2102/clas12/asportes/2N_Analysis_Truth_Samples/" + TARGET + "/" + GENIE_TUNE + "/Q2_th_test_samples/" + BEAM_E;
+    TString TRUTH_SAMPLE_ROOT_FILE_PREFIX = TARGET + "_" + GENIE_TUNE + "_Q2_" + Q2_CUT + "_" + BEAM_E;
+    TString TRUTH_SAMPLE_ROOT_FILE = TRUTH_SAMPLE_ROOT_FILE_PREFIX + ".root";
 
-  TString RECO_SAMPLES_TOPDIR = "/lustre24/expphy/volatile/clas12/asportes/2N_Analysis_Reco/2N_Analysis_Reco_Samples";
-  TString RECO_SAMPLES_SUBDIR = "master-routine_validation_01-eScattering";
-  TString RECO_SAMPLES_LUNDDIR = "lundfiles";
+    TString RECO_SAMPLES_TOPDIR = "/lustre24/expphy/volatile/clas12/asportes/2N_Analysis_Reco/2N_Analysis_Reco_Samples";
+    TString RECO_SAMPLES_SUBDIR = "master-routine_validation_01-eScattering";
+    TString RECO_SAMPLES_LUNDDIR = "lundfiles";
 
-  gSystem->Exec("mkdir -p " + RECO_SAMPLES_TOPDIR + "/" + TARGET);
-  gSystem->Exec("mkdir -p " + RECO_SAMPLES_TOPDIR + "/" + TARGET + "/" + GENIE_TUNE);
-  gSystem->Exec("mkdir -p " + RECO_SAMPLES_TOPDIR + "/" + TARGET + "/" + GENIE_TUNE + "/Q2_th_test_samples");
+    gSystem->Exec("mkdir -p " + RECO_SAMPLES_TOPDIR + "/" + TARGET);
+    gSystem->Exec("mkdir -p " + RECO_SAMPLES_TOPDIR + "/" + TARGET + "/" + GENIE_TUNE);
+    gSystem->Exec("mkdir -p " + RECO_SAMPLES_TOPDIR + "/" + TARGET + "/" + GENIE_TUNE + "/Q2_th_test_samples");
 
-  TString RECO_SAMPLE_OUTPUT_DIR = RECO_SAMPLES_TOPDIR + "/" + TARGET + "/" + GENIE_TUNE + "/Q2_th_test_samples/" + BEAM_E;
+    TString RECO_SAMPLE_OUTPUT_DIR = RECO_SAMPLES_TOPDIR + "/" + TARGET + "/" + GENIE_TUNE + "/Q2_th_test_samples/" + BEAM_E;
 
-  gSystem->Exec("rm -rf " + RECO_SAMPLE_OUTPUT_DIR);
-//  gSystem->Exec("mkdir -p " + RECO_SAMPLE_OUTPUT_DIR);
-//  gSystem->Exec("mkdir -p " + RECO_SAMPLE_OUTPUT_DIR + "/lundfiles");
-//  gSystem->Exec("mkdir -p " + RECO_SAMPLE_OUTPUT_DIR + "/mchipo");
-//  gSystem->Exec("mkdir -p " + RECO_SAMPLE_OUTPUT_DIR + "/reconhipo");
+    gSystem->Exec("rm -rf " + RECO_SAMPLE_OUTPUT_DIR);
+    //  gSystem->Exec("mkdir -p " + RECO_SAMPLE_OUTPUT_DIR);
+    //  gSystem->Exec("mkdir -p " + RECO_SAMPLE_OUTPUT_DIR + "/lundfiles");
+    //  gSystem->Exec("mkdir -p " + RECO_SAMPLE_OUTPUT_DIR + "/mchipo");
+    //  gSystem->Exec("mkdir -p " + RECO_SAMPLE_OUTPUT_DIR + "/reconhipo");
 
-  GENIE_to_LUND((TRUTH_SAMPLE_INPUT_DIR + "/" + RECO_SAMPLES_SUBDIR + "/" + TRUTH_SAMPLE_ROOT_FILE),
-                RECO_SAMPLE_OUTPUT_DIR,
-//                (RECO_SAMPLE_OUTPUT_DIR + "/" + RECO_SAMPLES_LUNDDIR),
-//                RECO_SAMPLE_OUTPUT_DIR,
-                TRUTH_SAMPLE_ROOT_FILE_PREFIX,
-                NUM_OF_FILES,
-                TARGET_TYPE,
-                TARGET_A,
-                TARGET_Z,
-                0.03,
-                0.1,
-                0.02);
-
+    GENIE_to_LUND((TRUTH_SAMPLE_INPUT_DIR + "/" + RECO_SAMPLES_SUBDIR + "/" + TRUTH_SAMPLE_ROOT_FILE),
+                  RECO_SAMPLE_OUTPUT_DIR,
+                  //                (RECO_SAMPLE_OUTPUT_DIR + "/" + RECO_SAMPLES_LUNDDIR),
+                  //                RECO_SAMPLE_OUTPUT_DIR,
+                  TRUTH_SAMPLE_ROOT_FILE_PREFIX,
+                  NUM_OF_FILES,
+                  TARGET_TYPE,
+                  TARGET_A,
+                  TARGET_Z,
+                  0.2,
+                  0.4,
+                  0.1);
 }
