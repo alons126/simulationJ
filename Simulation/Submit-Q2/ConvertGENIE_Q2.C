220207dd2949a29457d4b696e6e338c81b45135a
alons126 <alonsportes@mail.tau.ac.il>
2024-09-26 14:55:03 +0300

    Q2 test

--- Simulation/Submit-Q2/ConvertGENIE_Q2.C
+++ Simulation/Submit-Q2/ConvertGENIE_Q2.C
@@ -57,7 +57,7 @@ void ConvertGENIE_Q2()
                   TARGET_A,
                   TARGET_Z,
                   0.05,
-                  0.15,
+                  0.1,
                   0.05);
                 //   0.2,
                 //   0.4,
