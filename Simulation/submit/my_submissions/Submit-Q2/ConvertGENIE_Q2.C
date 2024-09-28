1d0abfa37e9ba4692fbcd67665d9ab20bf315bcb
alons126 <alonsportes@mail.tau.ac.il>
2024-09-28 17:22:09 +0300

    convert C12 6GeV with varing Q2 cuts G18

--- Simulation/submit/my_submissions/Submit-Q2/ConvertGENIE_Q2.C
+++ Simulation/submit/my_submissions/Submit-Q2/ConvertGENIE_Q2.C
@@ -18,9 +18,9 @@ void ConvertGENIE_Q2()
     // TString GENIE_TUNE = "GEM21_11a_00_000";
     TString GENIE_TUNE = "G18_10a_00_000";
     TString Q2_CUT = "def_Q2_th"; // 4029MeV_def_Q2_th
-    // TString Q2_CUT = "Q2_0_40"; // 598636MeV_Q2_0_4_th
+    // TString Q2_CUT = "Q2_0_40"; // 5986MeV_Q2_0_4_th
     TString BEAM_E = "4029MeV"; // 4029MeV_def_Q2_th
-    // TString BEAM_E = "5986MeV"; // 598636MeV_Q2_0_4_th
+    // TString BEAM_E = "5986MeV"; // 5986MeV_Q2_0_4_th
 
     int NUM_OF_FILES = 10;
     string TARGET_TYPE = "1-foil";
@@ -35,9 +35,17 @@ void ConvertGENIE_Q2()
     TString TRUTH_SAMPLE_INPUT_DIR = "/w/hallb-scshelf2102/clas12/asportes/2N_Analysis_Truth_Samples/" + TARGET + "/" + GENIE_TUNE +
                                      "/Q2_th_test_samples/small_Q2_test_samples/4029MeV_def_Q2_th";
 
-    // 598636MeV_Q2_0_4_th (G18):
+    // 5986MeV_Q2_0_4_th (G18):
+    TString TRUTH_SAMPLE_INPUT_DIR = "/w/hallb-scshelf2102/clas12/asportes/2N_Analysis_Truth_Samples/" + TARGET + "/" + GENIE_TUNE +
+                                     "/Q2_th_test_samples/small_Q2_test_samples/5986MeV_Q2_0_4_th";
+
+    // 4029MeV_def_Q2_th (SuSa):
     // TString TRUTH_SAMPLE_INPUT_DIR = "/w/hallb-scshelf2102/clas12/asportes/2N_Analysis_Truth_Samples/" + TARGET + "/" + GENIE_TUNE +
-    //                                  "/Q2_th_test_samples/small_Q2_test_samples/598636MeV_Q2_0_4_th";
+    //                                  "/Q2_th_test_samples/small_Q2_test_samples/4029MeV_def_Q2_th";
+
+    // 5986MeV_Q2_0_4_th (SuSa):
+    // TString TRUTH_SAMPLE_INPUT_DIR = "/w/hallb-scshelf2102/clas12/asportes/2N_Analysis_Truth_Samples/" + TARGET + "/" + GENIE_TUNE +
+    //                                  "/Q2_th_test_samples/small_Q2_test_samples/5986MeV_Q2_0_4_th";
 
     // TString TRUTH_SAMPLE_ROOT_FILE_PREFIX = TARGET + "_" + GENIE_TUNE + "_Q2_" + Q2_CUT + "_" + BEAM_E;
     TString TRUTH_SAMPLE_ROOT_FILE_PREFIX = TARGET + "_" + GENIE_TUNE + "_" + Q2_CUT + "_" + BEAM_E;
@@ -57,22 +65,34 @@ void ConvertGENIE_Q2()
 
     gSystem->Exec("rm -rf " + RECO_SAMPLE_OUTPUT_DIR);
 
-    GENIE_to_LUND_Q2(TARGET, GENIE_TUNE, BEAM_E,
-                     //  "/Users/alon/Downloads/e_on_1000060120_2070MeV_0.gst.root",
-                     (TRUTH_SAMPLE_INPUT_DIR + "/" + RECO_SAMPLES_SUBDIR + "/" + TRUTH_SAMPLE_ROOT_FILE),
-                     RECO_SAMPLE_OUTPUT_DIR,
-                     TRUTH_SAMPLE_ROOT_FILE_PREFIX,
-                     NUM_OF_FILES,
-                     TARGET_TYPE,
-                     TARGET_A,
-                     TARGET_Z,
-                     //  0.05,  // start
-                     //  0.15,  // finish
-                     //  0.05); // delta
-                     0.02,  // start (4029MeV_def_Q2_th)
-                     0.4,   // finish (4029MeV_def_Q2_th)
-                     0.01); // delta (4029MeV_def_Q2_th)
-                            //  0.4,  // start (598636MeV_Q2_0_4_th)
-                            //  0.8,  // finish (598636MeV_Q2_0_4_th)
-                            //  0.1); // delta (598636MeV_Q2_0_4_th)
+    std::string beam_e = BEAM_E.Data();
+
+    if (beam_e == "4029MeV")
+    {
+        GENIE_to_LUND_Q2(TARGET, GENIE_TUNE, BEAM_E,
+                         (TRUTH_SAMPLE_INPUT_DIR + "/" + RECO_SAMPLES_SUBDIR + "/" + TRUTH_SAMPLE_ROOT_FILE),
+                         RECO_SAMPLE_OUTPUT_DIR,
+                         TRUTH_SAMPLE_ROOT_FILE_PREFIX,
+                         NUM_OF_FILES,
+                         TARGET_TYPE,
+                         TARGET_A,
+                         TARGET_Z,
+                         0.02,  // start (4029MeV_def_Q2_th)
+                         0.4,   // finish (4029MeV_def_Q2_th)
+                         0.01); // delta (4029MeV_def_Q2_th)
+    }
+    else if (beam_e == "5986MeV")
+    {
+        GENIE_to_LUND_Q2(TARGET, GENIE_TUNE, BEAM_E,
+                         (TRUTH_SAMPLE_INPUT_DIR + "/" + RECO_SAMPLES_SUBDIR + "/" + TRUTH_SAMPLE_ROOT_FILE),
+                         RECO_SAMPLE_OUTPUT_DIR,
+                         TRUTH_SAMPLE_ROOT_FILE_PREFIX,
+                         NUM_OF_FILES,
+                         TARGET_TYPE,
+                         TARGET_A,
+                         TARGET_Z,
+                         0.4,  // start (5986MeV_Q2_0_4_th)
+                         0.8,  // finish (5986MeV_Q2_0_4_th)
+                         0.01); // delta (5986MeV_Q2_0_4_th)
+    }
 }
