9cb807400d39c02f3afdb510723c51cf88b7e506
alons126 <alonsportes@mail.tau.ac.il>
2025-02-05 13:22:03 +0200

    RGM_lAr_test_sample_4GeV_6

--- Simulation/submit/my_submissions/Ar40_imp_tests/submit_GEMC_RGM_lAr.sh
+++ Simulation/submit/my_submissions/Ar40_imp_tests/submit_GEMC_RGM_lAr.sh
@@ -3,14 +3,15 @@
 #SBATCH --ntasks=1                                                                                                   
 #SBATCH --mem-per-cpu=2000                                                                                            
 #SBATCH --account=clas12                                                                                             
-#SBATCH --job-name=RGM_lAr_test_sample_4GeV_5                                                                                             
+#SBATCH --job-name=RGM_lAr_test_sample_4GeV_6                                                                                             
 #SBATCH --partition=production                                                               
 #SBATCH --time=20:00:00                                                                                               
 #SBATCH --output=/farm_out/%u/%x-%j-%N.out                                                                           
 #SBATCH --error=//farm_out/%u/%x-%j-%N.err                                                                           
 #SBATCH --array=1-10 #Number of files 1-N                                                                                                 
 
-echo "$GEMC_DATA_DIR"
+echo "GEMC_DATA_DIR = $GEMC_DATA_DIR"
+echo
 
 NEVENTS=10000
 #-1.0 for inbending(6,4 GeV) 0.5 for outbending (2 Gev)
@@ -39,10 +40,25 @@ echo
 LUNDOUT=${OUTPATH}/lundfiles
 MCOUT=${OUTPATH}/mchipo
 RECONOUT=${OUTPATH}/reconhipo
+rm -rf ${RECONOUT}/*
 
 #SUBMIT GEMC MC
+echo
+echo
+echo "============================================================================================="
+echo "RUNNING GEMC"
+echo "============================================================================================="
+echo
+echo
 gemc -USE_GUI=0  -SCALE_FIELD="binary_torus, $TORUS" -SCALE_FIELD="binary_solenoid, -1.0" -N=$NEVENTS -INPUT_GEN_FILE="lund, ${LUNDOUT}/${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}.txt" -OUTPUT="hipo, ${MCOUT}/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus$TORUS.hipo" $GCARD
 # gemc -USE_GUI=0  -SCALE_FIELD="TorusSymmetric, $TORUS" -SCALE_FIELD="clas12-newSolenoid, -1.0" -N=$NEVENTS -INPUT_GEN_FILE="lund, ${LUNDOUT}/${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}.txt" -OUTPUT="hipo, ${MCOUT}/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus$TORUS.hipo" $GCARD
 
 #RECONSTRUCTION
+echo
+echo
+echo "============================================================================================="
+echo "RUNNING COTJAVA"
+echo "============================================================================================="
+echo
+echo
 recon-util -y $YAML -n $NEVENTS -i ${MCOUT}/mc_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo -o ${RECONOUT}/recon_${FILE_PREFIX}_${SLURM_ARRAY_TASK_ID}_torus${TORUS}.hipo
