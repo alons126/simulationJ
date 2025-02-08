68a08f11312c161d13b1852360694db4c9390a1f
alons126 <alonsportes@mail.tau.ac.il>
2025-02-08 18:21:15 +0200

    uniform_ConstPn 6GeV redo

--- Simulation/submit/my_submissions/Ar40_imp_tests/submit_GEMC_RGM_2_C_v2_L.sh
+++ Simulation/submit/my_submissions/Ar40_imp_tests/submit_GEMC_RGM_2_C_v2_L.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=20:00:00                                                                                               
 #SBATCH --output=/farm_out/%u/%x-%j-%N.out                                                                           
 #SBATCH --error=//farm_out/%u/%x-%j-%N.err                                                                           
-#SBATCH --array=0-9 #Number of files 1-N                                                                                                 
+#SBATCH --array=1-10 #Number of files 1-N                                                                                                 
 
 echo "GEMC_DATA_DIR = $GEMC_DATA_DIR"
 echo
