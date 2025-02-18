791afcf94cd48974b4b329578071c313f2a90963
alons126 <alonsportes@mail.tau.ac.il>
2025-02-18 10:08:16 +0200

    test 11

--- Simulation/submit/my_submissions/Ar40_imp_tests/submit_GEMC_RGM_2_C_v2_S.sh
+++ Simulation/submit/my_submissions/Ar40_imp_tests/submit_GEMC_RGM_2_C_v2_S.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1                                                                                                   
 #SBATCH --mem-per-cpu=2000                                                                                            
 #SBATCH --account=clas12                                                                                             
-#SBATCH --job-name=RGM_2_C_v2_S_sample_4GeV_test_10                                                                                             
+#SBATCH --job-name=RGM_2_C_v2_S_sample_4GeV_test_11                                                                                             
 #SBATCH --partition=production                                                               
 #SBATCH --time=20:00:00                                                                                               
 #SBATCH --output=/farm_out/%u/%x-%j-%N.out                                                                           
